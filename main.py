94214ab29798a48ae596703ca941ddd1f983dc10
clevenielson <clevenielson.vieira@gmail.com>
2023-12-18 08:10:43 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+
+display_messages = [
+        'Seja Feliz :)',
+        'Fique tranquilo, tudo vai acabar bem!'
+    ]
+
+while True:
+    resposta = input('Deseja receber um conselho? S|N: ')
+    if (resposta == 'S' or resposta == 's'):
+        mensagem = random.choice(display_messages)
+        print(mensagem)
+        print()
+
